daf25eef03cde384d5c8f1b02d578246efa7f6b3
Rafal <rostkos@gmail.com>
2020-03-08 11:50:01 +0100

    mapy

---
+++ src/pl/sda/school/Main.java
@@ -0,0 +1,34 @@
+package pl.sda.school;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        School school = new School("Ekonomincza", "Toruń");
+        School school1 = new School("Zawodowa", "Chełmża");
+        School school2 = new School("Informatyczna", "Warszawa");
+        School school3 = new School("Handlowa", "Elbląg");
+
+        Student student = new Student("Jan", "Kowalski");
+        Student student1 = new Student("Adam", "Nowak");
+        Student student2 = new Student("Witek", "Drwal");
+        Student student3 = new Student("Antoni", "Bomba");
+        Student student4 = new Student("Edek", "Spaw");
+
+
+
+        List<Student> studentLista= new ArrayList<>();
+
+
+
+
+
+
+
+    }
+}
---
+++ src/pl/sda/school/Student.java
@@ -0,0 +1,34 @@
+package pl.sda.school;
+
+public class Student {
+
+    private String name;
+    private String lastname;
+
+    public Student(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
+
+
+
+
+
